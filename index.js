6a846cceed673c68634f0b0cd12823efe61c2465
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-03-15 17:38:57 -0700

    added event listeners to change direction using arrowkeys

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
-
-
+// ADDING CHARACTER TO PAGE
+const character = newImage('assets/green-character/static.gif');
+// FUNCTION FOR CHANGING CHARACTER SRC WHILE WALKING
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+// CALLING MOVE FUNCTION WITH MOVEWITHKEYS FUNCTION CHAINED
+move(character).withKeys(100, 250, handleDirectionChange);
+// ADDING ELEMENTS TO PAGE
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
@@ -13,3 +32,7 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
+
+
--- move.js
+++ move.js
@@ -1,12 +1,70 @@
+// MOVE FUNCTION
 function move(element) {
+    // SETTING ELEMENT POSITION TO FIXED
     element.style.position = 'fixed'
-
+    // FUCNTION TO HANDLE ELEMENT COORDINATES (LEFT, BOTTOM)
     function moveToCoordinates(left, bottom) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
     }
-
+    // FUNCTION TO MOVE CHARACTER WITH ARROW KEYS
+    function moveWithKeys(left, bottom, handleChange) {
+        // SETTING DEFAULT DIRECTION TO NULL
+        let direction = null;
+        // LEFT AND BOTTOM WILL BE SET TO VARIABLES X,Y
+        let x = left;
+        let y = bottom;
+        // ADDING POSITION TO ELEMENT
+        element.style.left = x + 'px';
+        element.style.bottom = y + 'px';
+        // FUNCTION TO CHANGE VARIABLES X AND Y TO CHANGE DIRECTION
+        function moveCharacter(){ 
+            if(direction === 'west'){
+                x-=1
+            }
+            if(direction === 'north'){
+                y+=1
+            }
+            if(direction === 'east'){
+                x+=1
+            }
+            if(direction === 'south'){
+                y-=1
+            }
+            element.style.left = x + 'px';
+            element.style.bottom = y + 'px';
+        }
+        // RUNNING MOVECHARACTER EVERY SEC.
+        setInterval(moveCharacter, 1)
+        // ADDING KEYDOWN EVENT LISTENER TO HANDLE DIRECTION CHANGE
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west';
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north';
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east';
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south';
+            }
+            // CALLING CALLBACK FUNCTION
+            handleChange(direction);
+        });
+        // ADDING KEYUP EVENTLISTENER TO HANDLE WHEN KEY IS NOT PRESSED
+        document.addEventListener('keyup', function(e){
+            direction = null;
+            // CALLLING CALLBACK FUNCTION
+            handleChange(direction);
+        });
+    };
+    // RETURNING FUNCTIONS USING OBJECTS
     return {
-        to: moveToCoordinates
-    }
-}
+        to: moveToCoordinates,
+        withKeys: moveWithKeys
+    };
+};
